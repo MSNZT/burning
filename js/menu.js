63dfc2de0160481652c12b52ee496f8d0bd8bee6
MSNZT <shamyphy@gmail.com>
2022-09-08 22:05:00 +0300

    Updates

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 const toggle = document.querySelector(".navbar__toggle");
 const nav = document.querySelector(".navbar");
 const body = document.querySelector("body");
+const navLink = document.querySelectorAll(".navbar__link");
 
 toggle.addEventListener("click", navToggle);
 
@@ -9,3 +10,12 @@ function navToggle() {
   toggle.classList.toggle("active");
   body.classList.toggle("lock");
 }
+
+navLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+    toggle.classList.remove("active");
+    body.classList.remove("lock");
+  }
+});
